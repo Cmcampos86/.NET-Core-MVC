b74e76d772ac22e664f3a9ba8c0716e615a1d697
Claudio Marcos de Campos <cmcampos86@gmail.com>
2024-03-16 15:47:06 -0300

    Estudos .Net Core MVC

---
+++ PrimeiraApp/Models/ModelsController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PrimeiraApp.Models;
+
+namespace PrimeiraApp.Controllers
+{
+    public class ModelsController : Controller
+    {
+        public IActionResult Index()
+        {
+            //var aluno = new Aluno();
+            var aluno = new Aluno()
+            {
+                Nome = "A",
+                Email = "eduardo",
+                EmailConfirmacao = "eduardo.com"
+            };
+
+            if (TryValidateModel(aluno))//Validação da Model
+            {
+                return View(aluno);
+            }
+
+            var ms = ModelState;//Retorna as validações da model
+
+            //Retorna a lista de erros
+            var erros = ModelState.Select(x => x.Value.Errors)
+                                        .Where(y => y.Count > 0)
+                                        .ToList();
+
+            //Mostra a lista de erros
+            erros.ForEach(r => Console.WriteLine(r.First().ErrorMessage));
+
+            return View();
+        }
+    }
+}
---
+++ PrimeiraApp/ViewComponents/SaudacaoAlunoViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PrimeiraApp.Models;
+
+namespace PrimeiraApp.ViewComponents
+{
+    //ViewComponents: Componentes independentes. São server-side e podem realizar ações como obter dados de uma tabela e exibir valores manipulados.
+    //Os ViewComponents podem serv recursos como carrinho de compras, paginação, barra de pesquisa e etc
+    //ViewComponents segue uma convenção: <Nome>ViewComponent e herda de ViewComponent
+    //Criar uma pasta dentro de Shared como Components
+
+    public class SaudacaoAlunoViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            // Pegar o aluno lá na base de dados!!!
+            // Pegar o dado (nome) do aluno que está logado!
+            var aluno = new Aluno() { Nome = "Eduardo" };
+
+            return View(aluno);
+        }
+    }
+}
