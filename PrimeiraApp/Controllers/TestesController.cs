1594e756ecf86db586f429beedb8d5e32357c9b9
Claudio Marcos de Campos <cmcampos86@gmail.com>
2024-03-16 14:31:43 -0300

    Estudos .Net Core MVC

--- PrimeiraApp/Controllers/TestesController.cs
+++ PrimeiraApp/Controllers/TestesController.cs
@@ -8,12 +8,22 @@ namespace PrimeiraApp.Controllers
     [Route("gestao-da-conta", Order = 2)]//Pode colocar uma ordem de chamada das rotas
     public class TestesController : Controller
     {
+        //Especificar os verbos
+        //Deve manter o nome do parâmetro que esta na rota.
+        //Pode alterar o nome ou na configuração da rota ou no parâmetro do verbo que esta a rota
+        //Caso seja passado mais um parâmetro na controller, este pode ser acesso como se fosse uma querystring (Ex: detalhes/1?teste=ola).
+        //O parâmetro passa a ser uma rota e não um parâmetro
+        //Exemplo de parâmetro opcional: [HttpGet("detalhes/{id:int}/{id2?}")] e na controller Details(int id, int id2 = 0) - Opcional
+        //Quando o verbo é post:
+            //Usando o Bind para especificar o campos do Form que eu quero: Create([Bind("Id,Nome,Email")] IFormCollection collection)
+            //Usando o FromForm vai pegar todas as informações do formulário: Create([FromForm] IFormCollection collection)
+
         public ActionResult Index()
         {
             return View();
         }
 
-        [HttpGet("detalhes/{id:int}")] //Especificar os verbos
+        [HttpGet("detalhes/{id:int}")] 
         public ActionResult Details(int id)
         {
             return View();
