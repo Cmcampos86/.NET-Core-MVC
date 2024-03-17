f1bf9a918999bd1daa8479500c6c90a577b62297
Claudio Marcos de Campos <cmcampos86@gmail.com>
2024-03-17 09:39:50 -0300

    Estudos .Net Core MVC

---
+++ PrimeiraApp/Controllers/TesteEFController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PrimeiraApp.Data;
+using PrimeiraApp.Models;
+
+namespace PrimeiraApp.Controllers
+{
+    public class TesteEFController : Controller
+    {
+        public AppDbContext Db { get; set; }
+
+        public TesteEFController(AppDbContext db)
+        {
+            Db = db;
+        }
+
+        public IActionResult Index()
+        {
+            var aluno = new Aluno()
+            {
+                Nome = "Eduardo",
+                Email = "hello@eduardo.com",
+                DataNascimento = new DateTime(1982, 04, 24),
+                Avaliacao = 5,
+                Ativo = true
+            };
+
+            #region Adicionar
+
+            //Db.Alunos.Add(aluno);
+            //Db.SaveChanges();
+
+            #endregion
+
+            #region Alterar
+
+            //var alunosChange = Db.Alunos.Where(a => a.Nome.Contains("Eduardo")).FirstOrDefault();
+            //alunosChange.Nome = "Eduardo Pires";
+
+            //Db.Alunos.Update(alunosChange);
+            //Db.SaveChanges();
+
+            #endregion
+
+            #region Excluir
+
+            //Db.Alunos.Remove(alunosChange);
+            //Db.SaveChanges();
+
+            #endregion
+
+            return Content("");
+        }
+    }
+}
